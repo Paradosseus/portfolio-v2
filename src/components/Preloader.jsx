c416c60d328fe63eeb62c8a807c16c2de99f6a89
Francis Joshua Collado <colladofrancis524@gmail.com>
2023-11-08 21:03:17 +0800

    Fixed resizing issues in preloader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -7,13 +7,13 @@ export const Preloader = () => {
     return (
         <motion.div className='flex justify-center items-center w-screen h-screen' animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ duration: 1 }}>
             <div className='text-center '>
-                <motion.img src={Logo} alt="" className='lg:w-[100px] md:w-[75px] sm:w-[50px] m-auto my-6'
+                <motion.img src={Logo} alt="" className='lg:w-[100px] md:w-[75px] w-[50px] m-auto my-6'
                     animate={{
                         rotate: 360,
                     }}
                     transition={{ ease: easeOut, delay: 0.5, duration: .5 }} />
 
-                <h1 className='font-bold text-[25px] uppercase md:text-[20px] sm:text-[15px]'>L o a d i n g</h1>
+                <h1 className='font-bold lg:text-[25px] uppercase md:text-[20px] text-[15px]'>L o a d i n g</h1>
             </div>
         </motion.div>
     )
